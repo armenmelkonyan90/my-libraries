da82351b2891627efe3b3bc4eb1f281f7557f0f5
Your Name <you@example.com>
2022-04-24 11:56:24 +0400

    lib for docker compose up

---
+++ vars/dockerComposeUp.groovy
@@ -0,0 +1,5 @@
+def dckerComposeUp() {
+	step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose.yml', option: [$class: 'StartService', scale: 1, service: 'server'], useCustomDockerComposeFile: true])
+        sleep 30
+        step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose.yml', option: [$class: 'StopService', service: 'server'], useCustomDockerComposeFile: false])
+}
