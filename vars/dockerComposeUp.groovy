275b7be7ccfdc4956d48f9e02177f24f377cb6d7
armenmelkonyan90 <93577909+armenmelkonyan90@users.noreply.github.com>
2022-04-24 14:33:06 +0400

    Update dockerComposeUp.groovy

--- vars/dockerComposeUp.groovy
+++ vars/dockerComposeUp.groovy
@@ -1,4 +1,6 @@
-def call() {
+def call(debug flask_debug) {
+	sh 'echo DEBUG=debug >> .env'
+        sh 'echo FLASK_DEBUG=flask_debug >> .env'
 	step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose.yml', option: [$class: 'StartService', scale: 1, service: 'server'], useCustomDockerComposeFile: true])
         sleep 30
         step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose.yml', option: [$class: 'StopService', service: 'server'], useCustomDockerComposeFile: false])
