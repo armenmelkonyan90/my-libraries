b89d0fe339dd90fd0122c6ffb17483d0ff240ec3
armenmelkonyan90 <93577909+armenmelkonyan90@users.noreply.github.com>
2022-04-24 12:24:00 +0400

    Update dockerComposeUp.groovy

--- vars/dockerComposeUp.groovy
+++ vars/dockerComposeUp.groovy
@@ -1,4 +1,4 @@
-def Compose() {
+def call() {
 	step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose.yml', option: [$class: 'StartService', scale: 1, service: 'server'], useCustomDockerComposeFile: true])
         sleep 30
         step([$class: 'DockerComposeBuilder', dockerComposeFile: 'docker-compose.yml', option: [$class: 'StopService', service: 'server'], useCustomDockerComposeFile: false])
